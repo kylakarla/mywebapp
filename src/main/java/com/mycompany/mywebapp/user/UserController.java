0c49cbf8799a6f61c8d5c4d2dc8d6760e919e1fa
karl-markus <karl.markus.saukand@gmail.com>
2025-12-02 23:11:53 +0200

    Add edit user

--- src/main/java/com/mycompany/mywebapp/user/UserController.java
+++ src/main/java/com/mycompany/mywebapp/user/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -33,6 +34,21 @@ public class UserController {
     public String saveUser(User user, RedirectAttributes ra) {
         service.save(user);
         ra.addFlashAttribute("message", "User has been saved successfully");
+        ra.addFlashAttribute("messageType", "success");
         return "redirect:/users";
+    }
+
+    @GetMapping("/users/edit/{id}")
+    public String showEditForm(@PathVariable("id") Integer id, Model model, RedirectAttributes ra) {
+        try  {
+            User user = service.get(id);
+            model.addAttribute("user", user);
+            model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+            return "user_form";
+        } catch (Exception ex) {
+            ra.addFlashAttribute("message", ex. getMessage());
+            ra.addFlashAttribute("messageType", "danger");
+            return "redirect:/users";
+        }
     }
 }
--- src/main/java/com/mycompany/mywebapp/user/UserService.java
+++ src/main/java/com/mycompany/mywebapp/user/UserService.java
@@ -17,4 +17,8 @@ public class UserService {
     public void save(User user) {
         repo.save(user);
     }
+
+    public User get(Integer id) {
+
+    }
 }
